3fd8c9575b9da7affe3caec8e1cc52ba37469ee9
unknown <bonchyvelasco@outlook.com>
2014-10-19 06:11:29 +0800

    idk yet

--- application/models/user.php
+++ application/models/user.php
@@ -29,6 +29,14 @@ class User extends CI_Model{
 		}
 	}
 
+	public function get_user() {
+		$this->db->select(array(
+			'user.userid as userid',
+			'user.username as name',
+			'user.about as about',
+		);
+	}
+	
 	public function get_all_users($isOrg = false){
 		$this->db->select(array(
 				'user.userid as userid',
@@ -43,7 +51,7 @@ class User extends CI_Model{
 		return $query->result();
 	}
 
-	public function update_user($userid, $userdata) {
+	public function update_user($userid, $password) {
 		$this->db->where('userid', $userid);
 		$this->db->update('user', $userdata);
 	}
--- application/views/users_edit.php
+++ application/views/users_edit.php
@@ -1,5 +1,17 @@
 <div id="main-content">
-    <div class="greeting">
-        USERS EDIT
+    <div class="segment">
+        <div class="greeting">
+            USER
+            <a class="side-link" href="#">[Edit]</a>
+        </div>
+            <?php foreach ($user) { ?>
+				<tr>
+					<td><?php echo $user->username ?></td>
+					<td><?php echo $user->password ?></td>
+                    <td><?php echo $about->about ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
