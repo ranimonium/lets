2c61c4abc6f55be7af0b6a4f8b322b8940c91c3e
emircado <emir.mercado@upd.edu.ph>
2014-10-19 00:53:02 +0800

    Emir 2

--- application/views/favors.php
+++ application/views/favors.php
@@ -1,5 +1,33 @@
 <div id="main-content">
-    <div class="greeting">
-        FAVORS
+
+    <div class="segment">
+        <div class="greeting">
+            FAVORS
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Worth</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($favors as $f) { ?>
+                    <tr>
+                        <td><?php echo $f->name ?></td>
+                        <td><?php echo $f->type ?></td>
+                        <td><?php echo $f->worth ?></td>
+                        <td><?php echo $f->description ?></td>
+                        <td><?php echo $f->qty ?></td>
+
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
